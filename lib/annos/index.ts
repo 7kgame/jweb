daa8f98b6d82c9a41af5cef355002e2cf026df80
cyij <cyij2006@gmail.com>
2019-04-18 18:17:36 +0800

    up

---
+++ lib/annos/index.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Post, Put, Patch, Options } from './controller'
+import ResponseBody from './response_body'
+
+export {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Patch,
+  Options,
+  ResponseBody
+}
---
+++ lib/annos/response_body.ts
@@ -0,0 +1,44 @@
+import { AnnotationType, annotationHelper, BeanFactory, CTOR_ID, getObjectType, isAsyncFunction } from 'jbean'
+import { Request, Response } from '../base'
+import { xmlEncode } from '../utils'
+
+export default function ResponseBody (component?: any, type?: any) {
+  return annotationHelper(arguments, callback)
+}
+
+const callback = function (annoType: AnnotationType, ctor: object | Function) {
+  if (annoType === AnnotationType.clz) {
+    BeanFactory.addBeanMeta(AnnotationType.clz, ctor, null, ResponseBody, [arguments[2]])
+  } else if (annoType === AnnotationType.method) {
+    BeanFactory.addBeanMeta(AnnotationType.method, ctor, arguments[2], ResponseBody, [arguments[4]])
+  }
+}
+
+ResponseBody.preCall = function (type: string, req: Request, res: Response) {
+  switch (type) {
+    case 'json':
+      res.type('application/json')
+      break
+    case 'xml':
+      res.type('application/xml')
+      break
+    default:
+      break
+  }
+}
+
+ResponseBody.postCall = function (ret: any, type: string, req: Request, res: Response) {
+  switch (type) {
+    case 'json':
+      if (typeof ret === 'object') {
+        ret = JSON.stringify(ret)
+      }
+      break
+    case 'xml':
+      ret = xmlEncode(ret)
+      break
+    default:
+      break
+  }
+  return ret
+}
