1a1313f13ec0f62e903be0fb061536bebcb2f590
wumingliang <wumingliang@7k7k.com>
2019-05-06 15:38:43 +0800

    return daoInit

--- lib/starters/index.ts
+++ lib/starters/index.ts
@@ -2,5 +2,5 @@ import Application from '../application'
 import daoInit from './dao'
 
 export default async function applicationStarter(application: Application) {
-  daoInit(application)
+  return daoInit(application)
 }
